6f04872ef9b4ac9c5e38a17082e06e0411d02ede
AlexandreGoular <alexandre.goulart@aluno.ifsp.edu.br>
2025-06-11 23:23:21 -0300

    Navbar create

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export function Navbar() {
+
+    
+    return (
+        <>
+
+        <a href="#">Home</a>
+        <a href="#">On</a>
+        <a href="#">Download</a>
+        <a href="#">Assentment</a>
+        <a href="#">Admin</a>    
+        
+        </>
+    )
+}
